3d74b573a21020de21c0c5c6c6be6c2e93955f23
ITSC <ITSC@DESKTOP-ONE4R81>
2022-11-21 21:51:23 +0900

    dqy04

--- src/day04/method/Mainclass01.java
+++ src/day04/method/Mainclass01.java
@@ -10,9 +10,14 @@ package day04.method;
  * 
  * }
  * 
+ * 오버로딩
+ *		메소드 명이 같아도, 매개변수 갯수 또는 타입이 다르면 사용가능
+ * 
  * 
  */
 public class Mainclass01 {
+	public static String name;	//전역변수 name 선언
+	
 	public static void main(String[] args) {
 		System.out.println("main method 시작");
 		
@@ -21,6 +26,14 @@ public class Mainclass01 {
 		
 		//printName 메소드 호출하기
 		PrintName("혜진");
+		setName("피카츄");
+		
+		String strName = getName();
+		String eMon = getEvolution("꼬부기");
+		
+		
+		System.out.println(eMon);
+		System.out.println("전역변수 name: "+strName);
 
 	}
 	//인자 x 리턴 x - 실행하고 끝나는 메소드
@@ -28,10 +41,52 @@ public class Mainclass01 {
 		System.out.println("Hello world");
 
 	}
-	
+	//인자(매개변수) O 리턴 X
 	public static void PrintName(String name) {
 		System.out.println(" 이름: "+ name);
 	}
 	
+	//오버로딩메서드	인자(매개변수)O 리턴 X
+	public static void printName(String name, int age) { //-> // 오버로드
+		System.out.println(" 이름: "+ name);
+		System.out.println(" 나이: "+ age);
+		
+	}
+	
+	//인자O 리턴X -> 매개변수를 전역변수 name에 대입
+	public static void setName(String name) {
+		Mainclass01.name = name;
+		
+	}
+	
+	//인자X 리턴 O -> 전역변수 name을 변환 한다.
+	public static String getName() {
+		return name;	//Mainclass01.name
+		
+		
+	}
+	
+	
+	public static String getEvolution(String mon) {
+		String eMon = "";
+		if(mon.equals("꼬부기")) {
+			eMon="어니부기";
+		}else if(mon.equals("어니부기")) {
+			eMon="거북왕";
+		}
+		
+		return eMon;
+		
+	}
+	
 	
 }
+
+
+
+
+
+
+
+
+
